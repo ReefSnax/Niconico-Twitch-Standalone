0bfdfcccef544f96110cc41293f765c08e4d0dbc
Snax <148016696+ReefSnax@users.noreply.github.com>
2025-07-14 23:57:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.TWITCH_CONFIG = {
   clientId: "yourTwitchClientID",
   clientSecret: "yourTwitchClientSecret",
   refreshToken: "yourRefreshToken",
-  fontSize: "30px",           // Set custom font size (e.g., "20px", "1.5em", "150%")
+  fontSize: "40px",           // Set custom font size (e.g., "20px", "1.5em", "150%")
   hideUsername: true,        // Hides usernames if true, shows them if false
   maxChars: 55,        // Maximum characters per message
 };
