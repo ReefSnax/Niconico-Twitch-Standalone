4b50d8355461321932b323218599ddfeef3625bc
Snax <148016696+ReefSnax@users.noreply.github.com>
2025-07-14 23:16:22 -0500

    NicoNico settings by default

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.TWITCH_CONFIG = {
   clientId: "yourTwitchClientID",
   clientSecret: "yourTwitchClientSecret",
   refreshToken: "yourRefreshToken",
-  fontSize: "24px",           // Set custom font size (e.g., "20px", "1.5em", "150%")
-  hideUsername: false,        // Hides usernames if true
+  fontSize: "30px",           // Set custom font size (e.g., "20px", "1.5em", "150%")
+  hideUsername: true,        // Hides usernames if true, shows them if false
 };
